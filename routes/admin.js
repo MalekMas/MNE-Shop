731cea981b349c47aae4596269955cfb4be32eaa
MalekMasoud <178850251+MalekMas@users.noreply.github.com>
2025-06-09 16:10:03 +0300

    Adding validators

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,7 @@ router.get("/add-product", isAuth, adminController.getAddProduct);
 router.get("/products", isAuth, adminController.getProducts);
 router.get("/edit-product/:ProductId", isAuth, adminController.getEditProduct);
 router.post("/edit-product", isAuth, adminController.postEditProduct);
-router.post("/add-product", isAuth, adminController.postAddProduct);
+router.post('/add-product',isAuth, productValidator.add, productValidate,adminController.postAddProduct);
 router.post("/delete-product", isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
